a992f5b0b5d1270c4172137db7a814c5fad68692
BlazingTwist <m.h.Freedom2@web.de>
2021-04-24 02:18:04 +0200

    initial commit

---
+++ blazingtwist/config/BTConfigUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace BlazingTwistConfigTools.blazingtwist.config {
+	[PublicAPI]
+	public static class BTConfigUtils {
+		/// <summary>
+		/// Loads a config from a file.
+		/// If an instance is provided, the config will be loaded to that instance
+		/// otherwise a new instance is created
+		/// </summary>
+		/// <param name="pathToFile">absolute path to the config file. '/' will be replaced with the DirectorySeparatorChar</param>
+		/// <param name="instance">optional - instance of the existing config</param>
+		/// <typeparam name="TConfigType">type of the config to load</typeparam>
+		/// <returns>instance of the loaded config</returns>
+		public static TConfigType LoadConfigFile<TConfigType>(string pathToFile, [CanBeNull] TConfigType instance) {
+			pathToFile = pathToFile.Replace('/', Path.DirectorySeparatorChar);
+			if (!File.Exists(pathToFile)) {
+				Debug.LogError("Config file not found at path: '" + pathToFile + "'");
+				return default;
+			}
+
+			try {
+				using (StreamReader reader = File.OpenText(pathToFile)) {
+					if (instance == null) {
+						return BTConfigTools.LoadConfig<TConfigType>(reader);
+					}
+
+					BTConfigTools.LoadConfig(reader, instance);
+					return instance;
+				}
+			} catch (Exception e) {
+				Debug.LogError("Failed to load config at path: '" + pathToFile + "', caught exception: " + e);
+				return default;
+			}
+		}
+	}
+}
---
+++ blazingtwist/config/ConfigNode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlazingTwistConfigTools.blazingtwist.config {
+	internal class ConfigNode {
+		public string key;
+		public string value;
+		public List<ConfigNode> listValues;
+
+		public override string ToString() {
+			var builder = new StringBuilder();
+			builder.Append("Key: ");
+			builder.Append(key ?? "null");
+
+			builder.Append(" | Value: ");
+			builder.Append(value ?? "null");
+
+			builder.Append(" | listValues: ");
+			if (listValues == null) {
+				builder.Append("null");
+			} else {
+				builder.Append("{Node: (");
+				builder.Append(string.Join("), Node: (", listValues.Select(node => node.ToString())));
+				builder.Append(")}");
+			}
+
+			return builder.ToString();
+		}
+	}
+}
